ab2eb535d3d596c5bedae682b3b7642180393a39
Afif267 <afif2672000@hotmail.com>
2024-03-07 19:19:56 +0100

    anomationss

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,16 @@
-'use client'
-import React from 'react';
-import { Box, Typography, Container } from '@mui/material';
-import Item from './Item'; 
-import { BoxData } from '../interface/boxdata';
-import {data} from './Item.data'; 
-import { useEffect, useState } from 'react';
+"use client";
+import React from "react";
+import { Box, Typography, Container } from "@mui/material";
+import Item from "./Item";
+import { BoxData } from "../interface/boxdata";
+import { data } from "./Item.data";
+import { useEffect, useState } from "react";
 
 const useDarkMode = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
-    const matchMedia = window.matchMedia('(prefers-color-scheme: dark)');
+    const matchMedia = window.matchMedia("(prefers-color-scheme: dark)");
     const handleChange = (e: MediaQueryListEvent) => {
       setIsDarkMode(e.matches);
     };
@@ -18,60 +18,74 @@ const useDarkMode = () => {
     // Initial check
     setIsDarkMode(matchMedia.matches);
 
-    matchMedia.addEventListener('change', handleChange);
+    matchMedia.addEventListener("change", handleChange);
 
-    return () => matchMedia.removeEventListener('change', handleChange);
+    return () => matchMedia.removeEventListener("change", handleChange);
   }, []);
 
   return isDarkMode;
 };
 const Hero: React.FC = () => {
-   const isDarkMode = useDarkMode();
-  const imageSrc = isDarkMode ? '../imagesdark/Mainlogo.png' : '../images/logo.png';
-    return (
-        <Container maxWidth="sm" sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            padding: '20px',
-            minHeight: '100vh',
-            backgroundColor: isDarkMode ? 'black' : 'white',
-        }}>
-            <Box
-                component="img"
-                sx={{
-                    width: '80%',
-                    maxWidth: '300px',
-                    marginBottom: '20px',
-                    animation: 'pulse 2s infinite',
-                    '@keyframes pulse': {
-                        '0%': {
-                            transform: 'scale(1)',
-                        },
-                        '50%': {
-                            transform: 'scale(1.1)',
-                        },
-                        '100%': {
-                            transform: 'scale(1)',
-                        },
-                    },
-                }}
-                src={imageSrc}
-                alt="main logo"
-            />
-            <Typography variant="h5" color={isDarkMode? "white":'black'} sx={{ textAlign: 'center', marginBottom: '20px' }}>
-                {`Visit us on social media`}
-            </Typography>
-            
-            {data.map((item: BoxData) => (
-                <Item
-                  key={item.id}
-                  data={item} 
-                />
-            ))}
-            
-        </Container>
-    );
-}
+  const isDarkMode = useDarkMode();
+  const imageSrc = isDarkMode
+    ? "../imagesdark/Mainlogo.png"
+    : "../images/logo.png";
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "20px",
+        minHeight: "100vh",
+        backgroundColor: isDarkMode ? "black" : "white",
+      }}
+    >
+      <Box
+        component="img"
+        sx={{
+          width: "80%",
+          maxWidth: "300px",
+          marginBottom: "20px",
+          animation: "pulse 2s infinite",
+          "@keyframes pulse": {
+            "0%": {
+              transform: "scale(1)",
+            },
+            "50%": {
+              transform: "scale(1.1)",
+            },
+            "100%": {
+              transform: "scale(1)",
+            },
+          },
+        }}
+        src={imageSrc}
+        alt="main logo"
+      />
+      <Typography
+        variant="h5"
+        color={isDarkMode ? "white" : "black"}
+        sx={{ textAlign: "center", marginBottom: "20px" }}
+      >
+        {`Visit us on social media`}
+      </Typography>
+
+      {data.map((item: BoxData) => (
+        <Item key={item.id} data={item} />
+      ))}
+      <Box sx={{ marginTop: "20px", marginBottom: "20px" }}>
+        <Typography
+          variant="body2"
+          color={isDarkMode ? "white" : "black"}
+          sx={{ textAlign: "center" }}
+        >
+          Copyright © 2024 A.I
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
 
 export default Hero;
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
-'use client'
-import React from 'react';
-import { Box, Button, Link, Stack, Tooltip } from '@mui/material';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import { useEffect, useState } from 'react';
-import { BoxData } from '@/interface/boxdata';
+"use client";
+import React from "react";
+import { Box, Button, Link, Stack, Tooltip } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import { useEffect, useState } from "react";
+import { BoxData } from "@/interface/boxdata";
 
 interface ItemProps {
   data: BoxData;
@@ -13,14 +13,14 @@ interface ItemProps {
 const useDarkMode = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
   useEffect(() => {
-    const matchMedia = window.matchMedia('(prefers-color-scheme: dark)');
+    const matchMedia = window.matchMedia("(prefers-color-scheme: dark)");
     const handleChange = (e: MediaQueryListEvent) => {
       setIsDarkMode(e.matches);
     };
     // Initial check
     setIsDarkMode(matchMedia.matches);
-    matchMedia.addEventListener('change', handleChange);
-    return () => matchMedia.removeEventListener('change', handleChange);
+    matchMedia.addEventListener("change", handleChange);
+    return () => matchMedia.removeEventListener("change", handleChange);
   }, []);
   return isDarkMode;
 };
@@ -28,60 +28,79 @@ const useDarkMode = () => {
 const Item: React.FC<ItemProps> = ({ data }) => {
   const isDarkMode = useDarkMode();
   const logoSrc = isDarkMode ? data.logoDark : data.logo;
-  const boxShadowColor = isDarkMode ? 'white' : 'black';
+  const boxShadowColor = isDarkMode ? "white" : "black";
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        background: 'linear-gradient(45deg, #0FA0E6)', 
-        borderRadius: '15px',
-        width: '100%',
-        height: '80px',
-        maxWidth: '300px',
-        marginBottom: '15px',
-        padding: '15px',
-        boxShadow: '8px 8px 16px rgba(0,0,0,0.2)',
-        transition: 'all 0.5s ease-in-out',
-        animation: 'Pulse 4s infinite',
-        '@keyframes Pulse': {
-          '0%, 100%': {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        background: "linear-gradient(45deg, #0FA0E6)",
+        borderRadius: "15px",
+        width: "100%",
+        height: "80px",
+        maxWidth: "300px",
+        marginBottom: "15px",
+        padding: "15px",
+        boxShadow: "8px 8px 16px rgba(0,0,0,0.2)",
+        transition: "all 0.5s ease-in-out",
+        animation: "Pulse 4s infinite",
+        "@keyframes Pulse": {
+          "0%, 100%": {
             boxShadow: `0px 8px 8px ${boxShadowColor}`,
           },
-          '50%': {
+          "50%": {
             boxShadow: `0px 0px 8px ${boxShadowColor}`,
           },
         },
-        '&:hover': {
-          boxShadow: '0 12px 24px rgba(0,0,0,0.5)',
-          transform: 'scale(1.1)',
-          transition: 'transform 0.2s ease-in-out',
+        "&:hover": {
+          boxShadow: "0 12px 24px rgba(0,0,0,0.5)",
+          transform: "scale(1.1)",
+          transition: "transform 0.2s ease-in-out",
+          "& img": {
+            // Classic animation for the image on hover
+            transform: "scale(1.1) rotate(360deg)",
+            transition: "transform 0.2s ease-in-out",
+          },
         },
-        '@media (min-width: 600px)': {
-          flexDirection: 'row',
-          justifyContent: 'space-evenly',
-          padding: '10px',
+        "@media (min-width: 600px)": {
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          padding: "10px",
         },
-        
       }}
     >
-      <img src={logoSrc} alt="Logo" style={{ height: 'auto', maxHeight: '50px', marginBottom: '2px', width: '130px' }} />
+      <img
+        src={logoSrc}
+        alt="Logo"
+        style={{
+          height: "auto",
+          maxHeight: "50px",
+          marginBottom: "2px",
+          width: "130px",
+        }}
+      />
       <Stack direction="row" gap={4}>
         <Tooltip title="Visit our Facebook page" arrow>
           <Link href={data.facebook} underline="none">
             <Button
               sx={{
-                minWidth: '0px',
-                padding: '0px',
-                '&:hover': {
-                  transform: 'rotate(360deg)',
-                  transition: 'transform 0.5s ease-in-out',
+                minWidth: "0px",
+                padding: "0px",
+                "&:hover": {
+                  transform: "rotate(360deg)",
+                  transition: "transform 0.5s ease-in-out",
                 },
               }}
             >
-              <FacebookIcon sx={{ color: 'blue', background: 'transparent', transform: 'scale(1.5)' }} />
+              <FacebookIcon
+                sx={{
+                  color: "blue",
+                  background: "transparent",
+                  transform: "scale(1.5)",
+                }}
+              />
             </Button>
           </Link>
         </Tooltip>
@@ -89,15 +108,23 @@ const Item: React.FC<ItemProps> = ({ data }) => {
           <Link href={data.instagram} underline="none">
             <Button
               sx={{
-                minWidth: '0px',
-                padding: '0px',
-                '&:hover': {
-                  transform: 'rotate(360deg)',
-                  transition: 'transform 0.5s ease-in-out',
+                minWidth: "0px",
+                padding: "0px",
+                "&:hover": {
+                  transform: "rotate(360deg)",
+                  transition: "transform 0.5s ease-in-out",
                 },
               }}
             >
-              <InstagramIcon sx={{ color: 'white',borderRadius:'20%',  background: 'linear-gradient(39deg, rgba(255,188,65,1) 0%, rgba(193,41,166,1) 18%, rgba(221,40,125,1) 63%, rgba(147,28,156,1) 89%, rgba(155,26,183,1) 100%)', transform: 'scale(1.15)' }} />
+              <InstagramIcon
+                sx={{
+                  color: "white",
+                  borderRadius: "20%",
+                  background:
+                    "linear-gradient(39deg, rgba(255,188,65,1) 0%, rgba(193,41,166,1) 18%, rgba(221,40,125,1) 63%, rgba(147,28,156,1) 89%, rgba(155,26,183,1) 100%)",
+                  transform: "scale(1.15)",
+                }}
+              />
             </Button>
           </Link>
         </Tooltip>
